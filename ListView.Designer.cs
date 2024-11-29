acb18eefb0c6b9355023720a93955226a4bd6d10
Nthtuoi <tuoin712@gmail.com>
2024-11-30 00:25:07 +0700

    BTWD

---
+++ ListView.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Winform_T5
+{
+    partial class ListView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvHoTen = new System.Windows.Forms.ListView();
+            this.colum1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lvHoTen
+            // 
+            this.lvHoTen.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colum1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvHoTen.FullRowSelect = true;
+            this.lvHoTen.GridLines = true;
+            this.lvHoTen.HideSelection = false;
+            this.lvHoTen.Location = new System.Drawing.Point(12, 37);
+            this.lvHoTen.Name = "lvHoTen";
+            this.lvHoTen.Size = new System.Drawing.Size(776, 188);
+            this.lvHoTen.TabIndex = 1;
+            this.lvHoTen.UseCompatibleStateImageBehavior = false;
+            this.lvHoTen.View = System.Windows.Forms.View.Details;
+            this.lvHoTen.SelectedIndexChanged += new System.EventHandler(this.lvHoTen_SelectedIndexChanged);
+            // 
+            // colum1
+            // 
+            this.colum1.Text = "LastName";
+            this.colum1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "FirstName";
+            this.columnHeader2.Width = 200;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Phone";
+            this.columnHeader3.Width = 200;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(418, 236);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(119, 43);
+            this.btnThem.TabIndex = 2;
+            this.btnThem.Text = "Thêm ";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(418, 309);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(119, 43);
+            this.btnXoa.TabIndex = 3;
+            this.btnXoa.Text = "xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(418, 377);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(119, 44);
+            this.btnSua.TabIndex = 4;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 268);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "LastName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 326);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "FirstName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 377);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Phone";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(110, 262);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(257, 26);
+            this.txtLastName.TabIndex = 8;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(110, 326);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(257, 26);
+            this.txtFirstName.TabIndex = 9;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(110, 377);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(257, 26);
+            this.txtPhone.TabIndex = 10;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.lvHoTen);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvHoTen;
+        private System.Windows.Forms.ColumnHeader colum1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtPhone;
+    }
+}
---
+++ ListView.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winform_T5
+{
+    public partial class ListView : Form
+    {
+        public ListView()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ListViewItem lv1 = new ListViewItem(txtLastName.Text);
+            lv1.SubItems.Add(txtFirstName.Text);
+            lv1.SubItems.Add(txtPhone.Text);
+
+            lvHoTen.Items.Add(lv1);
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            while (lvHoTen.SelectedItems.Count > 0) 
+            {
+                lvHoTen.Items.RemoveAt(lvHoTen.SelectedItems[0].Index);
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            if (lvHoTen.SelectedItems.Count > 0)
+            { 
+                ListViewItem lv1 = lvHoTen.SelectedItems[0];
+                lv1.SubItems[0].Text = txtLastName.Text;
+                lv1.SubItems[1].Text = txtFirstName.Text;
+                lv1.SubItems[2].Text = txtPhone.Text;   
+            }
+        }
+
+        private void lvHoTen_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lvHoTen.SelectedItems.Count > 0)
+            {
+                ListViewItem lv1 = lvHoTen.SelectedItems[0];
+                String LastName = lv1.SubItems[0].Text;
+                String FirstName = lv1.SubItems[1].Text;
+                String Phone = lv1.SubItems[2].Text;
+                txtLastName.Text = LastName;
+                txtFirstName.Text = FirstName;
+                txtPhone.Text = Phone;
+            }
+
+        }
+    }
+}
